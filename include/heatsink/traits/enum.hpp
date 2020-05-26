9c207d6f8185ca81134737cc2db66c07169e8fec
Wyatt <git.wquist@gmail.com>
2020-05-26 15:13:58 +0100

    allow for type conversion to enum values

--- include/heatsink/traits/enum.hpp
+++ include/heatsink/traits/enum.hpp
@@ -1,8 +1,30 @@
 #pragma once
 
+#include <type_traits>
+
 #include <heatsink/platform/gl.hpp>
 
 namespace heatsink::gl {
+	namespace detail {
+		// A `std::bool_constant`-like alias for `GLenum` constant values.
+		template<GLenum V>
+		using enum_constant = std::integral_constant<GLenum, V>;
+	}
+
+	/**
+	 * The OpenGL enumeration equivalent of the given primitive C type (for
+	 * example, `GLfloat` gives the constant `GL_FLOAT`). This also works for
+	 * array types, to retrieve the `VEC`/`MAT` constants. If the type has no
+	 * equivalent, this value will equal `GL_NONE`. Note that the `GLboolean`
+	 * type requires special handling; it will result in `GL_UNSIGNED_BYTE`, as
+	 * it is equivalent to `unsigned char`. To retrieve the `GL_BOOL*`
+	 * constants, use the built-in `bool` type.
+	 */
+	template<class>
+	struct make_enum : detail::enum_constant<GL_NONE> {};
+	template<class T>
+	constexpr auto make_enum_v = make_enum<T>::value;
+
 	/**
 	 * Determine whether the `GLenum` value (an enum type) represents an OpenGL
 	 * object type. Note that this includes all objects, including programs,
@@ -20,6 +42,49 @@ namespace heatsink::gl {
 }
 
 namespace heatsink::gl {
+	template<> struct make_enum<bool>           : detail::enum_constant<GL_BOOL> {};
+	template<> struct make_enum<GLbyte>         : detail::enum_constant<GL_BYTE> {};
+	template<> struct make_enum<GLshort>        : detail::enum_constant<GL_SHORT> {};
+	template<> struct make_enum<GLint>          : detail::enum_constant<GL_INT> {};
+	template<> struct make_enum<GLubyte>        : detail::enum_constant<GL_UNSIGNED_BYTE> {};
+	template<> struct make_enum<GLushort>       : detail::enum_constant<GL_UNSIGNED_SHORT> {};
+	template<> struct make_enum<GLuint>         : detail::enum_constant<GL_UNSIGNED_INT> {};
+	template<> struct make_enum<GLfloat>        : detail::enum_constant<GL_FLOAT> {};
+	template<> struct make_enum<GLdouble>       : detail::enum_constant<GL_DOUBLE> {};
+	template<> struct make_enum<bool[2]>        : detail::enum_constant<GL_BOOL_VEC2> {};
+	template<> struct make_enum<bool[3]>        : detail::enum_constant<GL_BOOL_VEC3> {};
+	template<> struct make_enum<bool[4]>        : detail::enum_constant<GL_BOOL_VEC4> {};
+	template<> struct make_enum<GLint[2]>       : detail::enum_constant<GL_INT_VEC2> {};
+	template<> struct make_enum<GLint[3]>       : detail::enum_constant<GL_INT_VEC3> {};
+	template<> struct make_enum<GLint[4]>       : detail::enum_constant<GL_INT_VEC4> {};
+	template<> struct make_enum<GLuint[2]>      : detail::enum_constant<GL_UNSIGNED_INT_VEC2> {};
+	template<> struct make_enum<GLuint[3]>      : detail::enum_constant<GL_UNSIGNED_INT_VEC3> {};
+	template<> struct make_enum<GLuint[4]>      : detail::enum_constant<GL_UNSIGNED_INT_VEC4> {};
+	template<> struct make_enum<GLfloat[2]>     : detail::enum_constant<GL_FLOAT_VEC2> {};
+	template<> struct make_enum<GLfloat[3]>     : detail::enum_constant<GL_FLOAT_VEC3> {};
+	template<> struct make_enum<GLfloat[4]>     : detail::enum_constant<GL_FLOAT_VEC4> {};
+	template<> struct make_enum<GLdouble[2]>    : detail::enum_constant<GL_DOUBLE_VEC2> {};
+	template<> struct make_enum<GLdouble[3]>    : detail::enum_constant<GL_DOUBLE_VEC3> {};
+	template<> struct make_enum<GLdouble[4]>    : detail::enum_constant<GL_DOUBLE_VEC4> {};
+	template<> struct make_enum<GLfloat[2][2]>  : detail::enum_constant<GL_FLOAT_MAT2> {};
+	template<> struct make_enum<GLfloat[2][3]>  : detail::enum_constant<GL_FLOAT_MAT2x3> {};
+	template<> struct make_enum<GLfloat[2][4]>  : detail::enum_constant<GL_FLOAT_MAT2x4> {};
+	template<> struct make_enum<GLfloat[3][2]>  : detail::enum_constant<GL_FLOAT_MAT3x2> {};
+	template<> struct make_enum<GLfloat[3][3]>  : detail::enum_constant<GL_FLOAT_MAT3> {};
+	template<> struct make_enum<GLfloat[3][4]>  : detail::enum_constant<GL_FLOAT_MAT3x4> {};
+	template<> struct make_enum<GLfloat[4][2]>  : detail::enum_constant<GL_FLOAT_MAT4x2> {};
+	template<> struct make_enum<GLfloat[4][3]>  : detail::enum_constant<GL_FLOAT_MAT4x3> {};
+	template<> struct make_enum<GLfloat[4][4]>  : detail::enum_constant<GL_FLOAT_MAT4> {};
+	template<> struct make_enum<GLdouble[2][2]> : detail::enum_constant<GL_DOUBLE_MAT2> {};
+	template<> struct make_enum<GLdouble[2][3]> : detail::enum_constant<GL_DOUBLE_MAT2x3> {};
+	template<> struct make_enum<GLdouble[2][4]> : detail::enum_constant<GL_DOUBLE_MAT2x4> {};
+	template<> struct make_enum<GLdouble[3][2]> : detail::enum_constant<GL_DOUBLE_MAT3x2> {};
+	template<> struct make_enum<GLdouble[3][3]> : detail::enum_constant<GL_DOUBLE_MAT3> {};
+	template<> struct make_enum<GLdouble[3][4]> : detail::enum_constant<GL_DOUBLE_MAT3x4> {};
+	template<> struct make_enum<GLdouble[4][2]> : detail::enum_constant<GL_DOUBLE_MAT4x2> {};
+	template<> struct make_enum<GLdouble[4][3]> : detail::enum_constant<GL_DOUBLE_MAT4x3> {};
+	template<> struct make_enum<GLdouble[4][4]> : detail::enum_constant<GL_DOUBLE_MAT4> {};
+
 	constexpr bool is_object(GLenum e) {
 		switch (e) {
 			case GL_BUFFER:
