730252e48521648c24e4da4062f21062a73edf13
Wyatt <git.wquist@gmail.com>
2020-05-27 18:30:07 +0100

    add pixel format support for packed types

--- include/heatsink/traits/format.hpp
+++ include/heatsink/traits/format.hpp
@@ -13,6 +13,8 @@ namespace heatsink::gl {
 	 * because they work on enums instead of types, they do not take a template
 	 * parameter and instead take an argument directly. This allows them to work
 	 * during runtime as well (which is more common in this library).
+	 *
+	 * FIXME: provide compressed texture support.
 	 */
 	class format_traits {
 	public:
@@ -23,11 +25,12 @@ namespace heatsink::gl {
 		 */
 		static constexpr bool is_sized(GLenum);
 		/**
-		 * Calculate the base (unsized) type of an OpenGL image format. This is
-		 * the type that would be used as the "pixel format" when loading a
-		 * texture with data (e.g., `GL_RGB` is the base type of `GL_RGB8`).
+		 * Check if the given image format is "packed". This means that the
+		 * components of the enum are all represented within a single datatype.
+		 * This means that the total pixel size is not (extent * sizeof(type)),
+		 * but simply sizeof(type).
 		 */
-		static constexpr GLenum remove_size(GLenum);
+		static constexpr bool is_packed(GLenum);
 
 		/**
 		 * Calculate the most appropriate type enum for an OpenGL image format.
@@ -36,7 +39,6 @@ namespace heatsink::gl {
 		 * `GL_UNSIGNED_SHORT` (even though most enum types can be used).
 		 */
 		static constexpr GLenum underlying_datatype(GLenum);
-
 		/**
 		 * Calculate the format "extent", or number of color channels in an
 		 * OpenGL image format. For example, `GL_RGB` has `3` channels. Note
@@ -44,6 +46,13 @@ namespace heatsink::gl {
 		 * channel, and depth-stencil formats to have two channels.
 		 */
 		static constexpr std::size_t extent(GLenum);
+
+		/**
+		 * Calculate the base (unsized) type of an OpenGL image format. This is
+		 * the type that would be used as the "pixel format" when loading a
+		 * texture with data (e.g., `GL_RGB` is the base type of `GL_RGB8`).
+		 */
+		static constexpr GLenum remove_size(GLenum);
 		/**
 		 * Calculate the reversed format for the given enumeration. Note that
 		 * this method also takes a datatype parameter; in some cases, a
@@ -66,118 +75,26 @@ namespace heatsink::gl {
 		return (underlying_datatype(e) != GL_NONE);
 	}
 
-	constexpr GLenum format_traits::remove_size(GLenum e) {
+	constexpr bool format_traits::is_packed(GLenum e) {
+		// Handle all formats by comparing only the resulting type. If it is
+		// "normal" (int, char, etc.), it is not packed since OpenGL represents
+		// the packed datatypes with special enumeration values.
+		e = underlying_datatype(e);
+		if (e == GL_NONE)
+			return false;
+
 		switch (e) {
-			case GL_RED:
-			case GL_R8:
-			case GL_R8_SNORM:
-			case GL_R16:
-			case GL_R16_SNORM:
-			case GL_R16F:
-			case GL_R32F:
-				return GL_RED;
-			case GL_RED_INTEGER:
-			case GL_R8I:
-			case GL_R8UI:
-			case GL_R16I:
-			case GL_R16UI:
-			case GL_R32I:
-			case GL_R32UI:
-				return GL_RED_INTEGER;
+			case GL_BYTE:
+			case GL_UNSIGNED_BYTE:
+			case GL_SHORT:
+			case GL_UNSIGNED_SHORT:
+			case GL_HALF_FLOAT:
+			case GL_INT:
+			case GL_UNSIGNED_INT:
+			case GL_FLOAT:
+				return false;
 
-			case GL_RG:
-			case GL_RG8:
-			case GL_RG8_SNORM:
-			case GL_RG16:
-			case GL_RG16_SNORM:
-			case GL_RG16F:
-			case GL_RG32F:
-				return GL_RG;
-			case GL_RG_INTEGER:
-			case GL_RG8I:
-			case GL_RG8UI:
-			case GL_RG16I:
-			case GL_RG16UI:
-			case GL_RG32I:
-			case GL_RG32UI:
-				return GL_RG_INTEGER;
-
-			case GL_RGB:
-			case GL_RGB4:
-			case GL_RGB5:
-			case GL_RGB8:
-			case GL_RGB8_SNORM:
-			case GL_RGB10:
-			case GL_RGB12:
-			case GL_RGB16:
-			case GL_RGB16_SNORM:
-			case GL_RGB16F:
-			case GL_RGB32F:
-				return GL_RGB;
-			case GL_RGB_INTEGER:
-			case GL_RGB8I:
-			case GL_RGB8UI:
-			case GL_RGB16I:
-			case GL_RGB16UI:
-			case GL_RGB32I:
-			case GL_RGB32UI:
-				return GL_RGB_INTEGER;
-
-			case GL_RGBA:
-			case GL_RGBA2:
-			case GL_RGBA4:
-			case GL_RGBA8:
-			case GL_RGBA8_SNORM:
-			case GL_RGBA12:
-			case GL_RGBA16:
-			case GL_RGBA16_SNORM:
-			case GL_RGBA16F:
-			case GL_RGBA32F:
-				return GL_RGBA;
-			case GL_RGBA_INTEGER:
-			case GL_RGBA8I:
-			case GL_RGBA8UI:
-			case GL_RGBA16I:
-			case GL_RGBA16UI:
-			case GL_RGBA32I:
-			case GL_RGBA32UI:
-				return GL_RGBA_INTEGER;
-
-			case GL_DEPTH_COMPONENT:
-			case GL_DEPTH_COMPONENT16:
-			case GL_DEPTH_COMPONENT24:
-			case GL_DEPTH_COMPONENT32:
-			case GL_DEPTH_COMPONENT32F:
-				return GL_DEPTH_COMPONENT;
-
-			case GL_STENCIL:
-			case GL_STENCIL_INDEX8:
-				return GL_STENCIL;
-
-			case GL_DEPTH_STENCIL:
-			case GL_DEPTH24_STENCIL8:
-			case GL_DEPTH32F_STENCIL8:
-				return GL_DEPTH_STENCIL;
-
-			case GL_BGR:             return GL_BGR;
-			case GL_BGR_INTEGER:     return GL_BGR_INTEGER;
-			case GL_BGRA:            return GL_BGRA;
-			case GL_BGRA_INTEGER:    return GL_BGRA_INTEGER;
-
-			case GL_R3_G3_B2:        return GL_RGB;
-			case GL_RGB5_A1:         return GL_RGBA;
-			case GL_RGB10_A2:        return GL_RGBA;
-			case GL_RGB10_A2UI:      return GL_RGBA_INTEGER;
-			case GL_R11F_G11F_B10F:  return GL_RGB;
-			case GL_RGB9_E5:         return GL_RGB;
-			case GL_RGB565:          return GL_RGB;
-
-			case GL_SRGB:            return GL_RGB;
-			case GL_SRGB8:           return GL_RGB;
-			case GL_SRGB_ALPHA:      return GL_RGBA;
-			case GL_SRGB8_ALPHA8:    return GL_RGBA;
-
-			default: return GL_NONE;
+			default: return true;
 		}
 	}
 
@@ -297,6 +214,121 @@ namespace heatsink::gl {
 				return 4; 
 
 			default: return 0;
+		}
+	}
+
+	constexpr GLenum format_traits::remove_size(GLenum e) {
+		switch (e) {
+			case GL_RED:
+			case GL_R8:
+			case GL_R8_SNORM:
+			case GL_R16:
+			case GL_R16_SNORM:
+			case GL_R16F:
+			case GL_R32F:
+				return GL_RED;
+			case GL_RED_INTEGER:
+			case GL_R8I:
+			case GL_R8UI:
+			case GL_R16I:
+			case GL_R16UI:
+			case GL_R32I:
+			case GL_R32UI:
+				return GL_RED_INTEGER;
+
+			case GL_RG:
+			case GL_RG8:
+			case GL_RG8_SNORM:
+			case GL_RG16:
+			case GL_RG16_SNORM:
+			case GL_RG16F:
+			case GL_RG32F:
+				return GL_RG;
+			case GL_RG_INTEGER:
+			case GL_RG8I:
+			case GL_RG8UI:
+			case GL_RG16I:
+			case GL_RG16UI:
+			case GL_RG32I:
+			case GL_RG32UI:
+				return GL_RG_INTEGER;
+
+			case GL_RGB:
+			case GL_RGB4:
+			case GL_RGB5:
+			case GL_RGB8:
+			case GL_RGB8_SNORM:
+			case GL_RGB10:
+			case GL_RGB12:
+			case GL_RGB16:
+			case GL_RGB16_SNORM:
+			case GL_RGB16F:
+			case GL_RGB32F:
+				return GL_RGB;
+			case GL_RGB_INTEGER:
+			case GL_RGB8I:
+			case GL_RGB8UI:
+			case GL_RGB16I:
+			case GL_RGB16UI:
+			case GL_RGB32I:
+			case GL_RGB32UI:
+				return GL_RGB_INTEGER;
+
+			case GL_RGBA:
+			case GL_RGBA2:
+			case GL_RGBA4:
+			case GL_RGBA8:
+			case GL_RGBA8_SNORM:
+			case GL_RGBA12:
+			case GL_RGBA16:
+			case GL_RGBA16_SNORM:
+			case GL_RGBA16F:
+			case GL_RGBA32F:
+				return GL_RGBA;
+			case GL_RGBA_INTEGER:
+			case GL_RGBA8I:
+			case GL_RGBA8UI:
+			case GL_RGBA16I:
+			case GL_RGBA16UI:
+			case GL_RGBA32I:
+			case GL_RGBA32UI:
+				return GL_RGBA_INTEGER;
+
+			case GL_DEPTH_COMPONENT:
+			case GL_DEPTH_COMPONENT16:
+			case GL_DEPTH_COMPONENT24:
+			case GL_DEPTH_COMPONENT32:
+			case GL_DEPTH_COMPONENT32F:
+				return GL_DEPTH_COMPONENT;
+
+			case GL_STENCIL:
+			case GL_STENCIL_INDEX8:
+				return GL_STENCIL;
+
+			case GL_DEPTH_STENCIL:
+			case GL_DEPTH24_STENCIL8:
+			case GL_DEPTH32F_STENCIL8:
+				return GL_DEPTH_STENCIL;
+
+			case GL_BGR:             return GL_BGR;
+			case GL_BGR_INTEGER:     return GL_BGR_INTEGER;
+			case GL_BGRA:            return GL_BGRA;
+			case GL_BGRA_INTEGER:    return GL_BGRA_INTEGER;
+
+			case GL_R3_G3_B2:        return GL_RGB;
+			case GL_RGB5_A1:         return GL_RGBA;
+			case GL_RGB10_A2:        return GL_RGBA;
+			case GL_RGB10_A2UI:      return GL_RGBA_INTEGER;
+			case GL_R11F_G11F_B10F:  return GL_RGB;
+			case GL_RGB9_E5:         return GL_RGB;
+			case GL_RGB565:          return GL_RGB;
+
+			case GL_SRGB:            return GL_RGB;
+			case GL_SRGB8:           return GL_RGB;
+			case GL_SRGB_ALPHA:      return GL_RGBA;
+			case GL_SRGB8_ALPHA8:    return GL_RGBA;
+
+			default: return GL_NONE;
 		}
 	}
 
